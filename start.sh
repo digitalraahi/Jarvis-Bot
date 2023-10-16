0d1f23e7a3e274cd06f1258dd1d1b7cf4462a7ff
digitalraahi <135437639+digitalraahi@users.noreply.github.com>
2023-10-17 02:09:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
-else
+  git clone https://github.com/digitalraahi/Jarvis-Bot.git /Jarvis-Bot
+else  
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Jarvis-Bot
 fi
-cd /VJ-FILTER-BOT 
+cd /Jarvis-Bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
